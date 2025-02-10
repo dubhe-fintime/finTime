a664da3b1633718e28924a40efea042691ff3421
gssong <dubhe.fintime@gmail.com>
2025-02-10 09:07:20 +0900

    유안타증권 주석 수정 대신증권 추가

--- corp/stock/dashinStock.py
+++ corp/stock/dashinStock.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-async def get209Data():
-    # 유안타증권 이벤트 페이지 URL
-    url = "https://www.myasset.com/myasset/hello/event/CU_0203000_T1.cmd"
-    detailUrl = "https://www.myasset.com"
+async def get267Data():
+    # 대신증권 이벤트 페이지 URL
+    url = "https://www.daishin.com/g.ds?m=1109&p=12931&v=12831"
+    detailUrl = "https://www.daishin.com"
 
     # 웹페이지 요청
     headers = {"User-Agent": "Mozilla/5.0"}
@@ -16,18 +16,18 @@ async def get209Data():
 
     #print(soup)
     # 이벤트 목록 찾기 (예제: 특정 클래스명을 기준으로 검색)
-    events = soup.find_all("div", class_="eventListWrap")
+    events = soup.find_all("div", class_="tabContent")
 
     # 이벤트 정보 출력
     event_list = []  # 이벤트 데이터를 담을 리스트
     for event in events:
         print(event)
-        li_tags = event.find_all("li")
+        li_tags = event.find_all("div", class_=["eventBox holding_pin", "eventBox"])
         #print(li_tags)
         print(f"이벤트 개수: {len(li_tags)}")  # 몇 개의 이벤트가 검색되는지 확인
         for li in li_tags:
-            title = li.find("p", class_="tit").text.strip() + " " + li.find("p", class_="txt").text.strip() # 이벤트 제목
-            date = li.find("em").text.strip()  # 이벤트 기간
+            title = li.find("p", class_="tit").text.strip() + " " + li.find("p", class_="sub").text.strip() # 이벤트 제목
+            date = li.find("p", class_="term").text.strip()  # 이벤트 기간
             link = li.find("a")["href"]  # 이벤트 상세 링크
             thumbnail = detailUrl + li.find('img')['src'] if li.find('img')['src'] else "" # 썸네일 이미지 링크
             link = detailUrl + li.find("a")["href"]  # 이벤트 상세 링크
