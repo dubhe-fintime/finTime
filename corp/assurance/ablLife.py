de725ec9cd5a662e0153dc3721e0a7f1040c575a
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-10 15:13:26 +0900

    CYH - 2개 보험사+db연결확인

--- corp/assurance/ablLife.py
+++ corp/assurance/ablLife.py
@@ -1,42 +1,67 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+##############################
+# 제목 : ABL생명보험
+# 금융사코드 : 437
+# 방식 : BeautifulSoup
+# 수집 데이터
+# 제목 : O | 시작일 : X | 종료일 : X | 썸네일 : O 
+# 이미지 : X | 내용 : X | 목록 URL : O | 상세 URL : O
+##############################
 
 async def get437Data():
-    # ABL생명 이벤트 페이지 URL
-    url = "https://www.abllife.co.kr/st/custDesk/events?page=index"
 
-    # 웹페이지 요청
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-
-    # HTML 파싱
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # 이벤트 목록 찾기
-    events = soup.find_all("div", class_="article_box thumb")
-
-    # 이벤트 정보 리스트
+    ######### 기초 설정 Start #############
+    # return 값 넣을 리스트
     event_list = []
-    print(f"이벤트 개수: {len(events)}")  # 몇 개의 이벤트가 검색되는지 확인
-    for event in events:
-        title_tag = event.find("a", class_="subject")  # 제목 태그 찾기
-        date_tag = event.find("span", class_="date")  # 날짜 태그 찾기
-        link_tag = event.find("a", class_="subject")  # 링크 태그 찾기
+    #URL
+    url = "https://www.abllife.co.kr/st/custDesk/events?page=index"
+    # 메인 URL 
+    domain = re.match(r"(https?://[^/]+)", url).group(1)
+    ######### 기초 설정 END ##############
+
+    try:
+        # 웹페이지 요청
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # 이벤트 목록 찾기
+        events = soup.find_all("div", class_="article_box thumb")
+        for event in events:
+            title_tag = event.find("a", class_="subject")  # 제목 태그 찾기
+            date_tag = event.find("span", class_="date")  # 날짜 태그 찾기
+            link_tag = event.find("a", class_="subject")  # 링크 태그 찾기
+
+            if title_tag and date_tag and link_tag:
+                title = title_tag.text.strip()  # 이벤트 제목
+                date = date_tag.text.strip()  # 이벤트 기간
+                link = link_tag["href"]  # 이벤트 상세 링크
+
+                ##TODO: 등록날짜는 있어서, 혹시몰라서 내비둠 
+                date_obj = datetime.strptime(date.replace(",", "").strip(), "%m월 %d일 %Y").strftime("%Y-%m-%d")
+
+                event_list.append({
+                    "title": title,
+                    "thumbNail": domain+event.find('img')['src'],
+                    "listURL": url,
+                    "detailURL": domain+link
+                })
+                # 확인용
+                # print(f"제목 : {title}")
+                # print(f"썸네일URL : {domain+event.find('img')['src']}")
+                # print(f"목록URL : {url}")
+                # print(f"상세URL: {domain+link}") 
+
+            print(f"ABL생명 크롤링 완료 | 이벤트 개수 : {len(event_list)}")
+            print("최종 결과 >>")
+            print(event_list)
+            return event_list       
         
-        if title_tag and date_tag and link_tag:
-            title = title_tag.text.strip()  # 이벤트 제목
-            date = date_tag.text.strip()  # 이벤트 기간
-            link = link_tag["href"]  # 이벤트 상세 링크
-
-            event_list.append({
-                "title": title,
-                "date": date,
-                "link": link
-            })
-
-            print(f"이벤트명: {title}")
-            print(f"이벤트 기간: {date}")
-            print(f"자세히 보기: {link}\n")
-
-    return event_list
+    except Exception as e:
+        print(f"ABL생명 오류 발생: {e}")
+        return e
---
+++ corp/assurance/miraeAssetLife.py
@@ -0,0 +1,83 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+##############################
+# 제목 : 미래에셋생명
+# 금융사코드 : 431
+# 방식 : API 
+# 수집 데이터
+# 제목 : O | 시작일 : O | 종료일 : O | 썸네일 : O 
+# 이미지 : X | 내용 : X | 목록 URL : O | 상세 URL : X
+##############################
+
+
+##############################
+# Service URL = "https://life.miraeasset.com/online/event/getEventList.do"
+# Method = POST
+        # headers = {
+        #     "Accept": "application/json, text/javascript, */*; q=0.01"
+        # }
+        # body = {
+        # "pageNo":1
+        # }
+##############################
+
+
+async def get431Data():
+    ######### 기초 설정 Start #############
+    # return 값 넣을 리스트
+    event_list = []
+    # API URL
+    url =  "https://life.miraeasset.com/online/event/getEventList.do"
+    # 메인 URL 
+    domain = re.match(r"(https?://[^/]+)", url).group(1)
+    # 이미지 파일 URL 기본값
+    img_domain =  "https://life.miraeasset.com/pool_img/"
+    #목록 URL
+    list_domain = "https://life.miraeasset.com/online/directPage.do?_OC_=4266&cp=MO-DR-040100-000000#MO-DR-040100-000000"
+    ######### 기초 설정 END ##############
+
+    try:
+        headers = {
+            "Accept": "application/json, text/javascript, */*; q=0.01"
+        }
+        data = {"pageNo":1}
+        # 웹페이지 요청
+        response = requests.post(url, headers=headers,json=data)
+        response.raise_for_status()  
+
+        #응답 데이터 가공
+        target_data = response.json()["data"]["rst"]
+        for element in target_data:
+            start_date, end_date = [re.sub(r"(\d{4})(\d{2})(\d{2})", r"\1-\2-\3", x) for x in [element['eventStartDtm'], element['eventEndDtm']]]
+
+            #  확인용
+            # print(f"제목 : {element['bbsTitle']}")
+            # print(f"시작 : {start_date}")
+            # print(f"종료 : {end_date}")
+            # print(f"썸네일URL : {img_domain+element['eventBanner']}")
+            # print(f"이벤트목록URL : {list_domain}")
+            # print("-")
+
+            event_list.append({
+                "title": element['bbsTitle'],
+                "startDt": start_date,
+                "endDt": end_date,
+                "thumbNail": img_domain+element['eventBanner'],
+                "listURL": list_domain
+            })
+
+        print(f"미래에셋생명 크롤링 완료 | 이벤트 개수 : {len(event_list)}")
+        print("최종 결과 >>")
+        print(event_list)
+        return event_list
+    
+    except Exception as e:
+        print(f"미래에셋생명 오류 발생: {e}")
+        return e
+
+
+
+
+
