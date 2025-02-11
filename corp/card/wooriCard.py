f456844a3173c2dbd0c4a448bbeeda8c95aee1f4
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-11 16:20:05 +0900

    우리카드SSL 오류 수정

--- corp/card/wooriCard.py
+++ corp/card/wooriCard.py
@@ -1,5 +1,7 @@
 import re
 import requests
+import ssl
+from requests.adapters import HTTPAdapter
 
 ##############################
 # 제목 : 우리카드
@@ -26,6 +28,14 @@ import requests
 # }
 ##############################
 
+class TLSAdapter(HTTPAdapter):
+    def init_poolmanager(self, *args, **kwargs):
+        context = ssl.create_default_context()
+        context.set_ciphers("DEFAULT@SECLEVEL=1")  # 보안 수준 낮춤
+        context.check_hostname = False  # SSL 인증서 검증 비활성화
+        kwargs["ssl_context"] = context
+        super().init_poolmanager(*args, **kwargs)
+
 
 async def get041Data():
     ######### 기초 설정 Start #############
@@ -40,13 +50,18 @@ async def get041Data():
     ######### 기초 설정 END ##############
 
     try:
+
         headers = {
             "Cookie": "JSESSIONID=t6QPpV1W9bQf5FvDL7p3OjvaaLjSNyy0tnsv0QnqDCdWR18EvjseTFgr6bGh6nKJ.amV1c19kb21haW4vd2NwYzEx; TS01d6fea6=01bae404f5e7fb0f8de5c45f60ce75a346c6934a5bb44d05190e4b06f76ab5aff11bc0feea45c91efe283656e9f9969d0f38938a82; _xm_webid_1_=1638786099; TS01bba15d=01bae404f5e7fb0f8de5c45f60ce75a346c6934a5bb44d05190e4b06f76ab5aff11bc0feea45c91efe283656e9f9969d0f38938a82; PCID=12f439a1-af00-60f3-c51b-9823509b4210-1739173354155; lang=ko; bodyYn=Y; _ga=GA1.1.1694652932.1739173355; _gcl_au=1.1.1519562618.1739173355; _ga_LXPH18QLPW=GS1.1.1739173354.1.1.1739173615.60.0.0"
         }
 
         data = {"bnf02PrgEvntVo":{"evntCtgrNo":"","searchKwrd":"","sortOrd":"orderNew","pageIndex":"1","pageSize":"15","evntItgCfcd":""}}
         # 웹페이지 요청
-        response = requests.post(url, headers=headers,json=data,verify=False)
+
+        session = requests.Session()
+        session.mount("https://", TLSAdapter())
+        response = session.post(url, headers=headers,json=data,verify=False)
+        
         response.raise_for_status()  
 
         #응답 데이터 가공
@@ -72,7 +87,10 @@ async def get041Data():
         return event_list
 
     except requests.exceptions.RequestException as e:
-        print(f"우리카드 오류 발생: {e}")
+        print(f"우리카드 요청 오류 발생: {e}")
+        return [{"ERROR": str(e)}]
+    except Exception as e:
+        print(f"우리카드 크롤링 오류 발생: {e}")
         return [{"ERROR": str(e)}]
 
 
