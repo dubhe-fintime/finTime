34f2a327b1937c46e61abbad8d7c3a7b6f57f719
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-12 08:56:11 +0900

    동양생명 오류 수정 완료

--- corp/assurance/dongyangLife.py
+++ corp/assurance/dongyangLife.py
@@ -1,7 +1,6 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-import traceback
 
 ##############################
 # 제목 : 동양생명
@@ -13,7 +12,7 @@ import traceback
 # 특이사항 : 이미지가 다운로드 URL
 ##############################
 
-def get402Data():
+async def get402Data():
     ######### 기초 설정 Start #############
 
     # return 값 넣을 리스트
@@ -26,43 +25,38 @@ def get402Data():
     detail_domain =  "https://online.myangel.co.kr/lounge/eventDetl.e1004?EVNT_SEQNO="
 
     ######### 기초 설정 END ##############
-    # 요청
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    response.raise_for_status()  # 오류 발생 시 예외 처리
-    soup = BeautifulSoup(response.text, 'html.parser')
 
-    
-    # 요소 찾기 
-    container = soup.find("div" ,class_="list")
-    for element in container.find_all("div",class_="list-item"):
-        print(element.find("div", class_="date-area").find("span"))
-        detail = detail_domain +element.find("a")["onclick"].split('(')[1].split(')')[0]
-        start_date= ""
-        end_date= ""
+    try :
+        # 요청
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+        response.raise_for_status()  # 오류 발생 시 예외 처리
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        #확인용
-        # print(f"제목 :{element.find('h4').text}")
-        # print(f"시작 :{start_date}")
-        # print(f"종료 :{end_date}")
-        # print(f"이미지URL :{domain+element.find('img')['src']} ")
-        # print(f"목록URL :{url}")
-        # print(f"상세URL :{detail}")
-
-
-        event_list.append({
-            "title": element.find('h4').text.strip(),
-            "startDt": start_date,
-            "endDt": end_date,
-            "thumbNail": domain+element.find('img')['src'],
-            "listURL": url,
-            "detailURL": detail
-        })
-
-
-
-    print(f"동양생명 크롤링 완료 | 이벤트 개수 : {len(event_list)}")
-    print("최종 결과 >>")
-    print(event_list)
-    return event_list
         
-get402Data()
+        # 요소 찾기 
+        container = soup.find("div" ,class_="list")
+        for element in container.find_all("div",class_="list-item"):
+            start_date, end_date = re.findall(r'\d{4}-\d{2}-\d{2}', element.find("div", class_="date-area").find("span").text.strip().replace(".","-"))
+            detail = detail_domain +element.find("a")["onclick"].split('(')[1].split(')')[0]
+
+
+            event_list.append({
+                "title": element.find('h4').text.strip(),
+                "startDt": start_date,
+                "endDt": end_date,
+                "thumbNail": domain+element.find('img')['src'],
+                "listURL": url,
+                "detailURL": detail
+            })
+
+
+
+        print(f"동양생명 크롤링 완료 | 이벤트 개수 : {len(event_list)}")
+        print("최종 결과 >>")
+        print(event_list)
+        return event_list
+    
+    except Exception as e:
+        print(f"KB카드 오류 발생: {e}")
+        return [{"ERROR": e}]
+            
