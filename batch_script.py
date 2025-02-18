358ba2ca8355ee2364ceebacffe0b3832b8072f2
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-19 08:20:52 +0900

    import

--- batch_script.py
+++ batch_script.py
@@ -4,7 +4,7 @@ import datetime
 import schedule
 import os
 import json
-from main import set_batch_log, set_batch_rst, del_batch_rst, set_user_mapp, set_batch_holiday, set_batch_end_log, app  # Flask 앱을 임포트
+from main import set_batch_log, set_batch_rst, del_batch_rst, set_user_mapp, set_batch_holiday, app  # Flask 앱을 임포트
 from main import test1, test2, test3, test4, test5, test6,test7,test8,test9,test10,test11,test12,test13
 from main import card1,card2,card3,card4,card5,card6
 from main import bank1,bank2,bank3,bank4,bank5,bank6,bank7,bank8
