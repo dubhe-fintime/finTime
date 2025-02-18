a91161cf7cf6089a912214c93a9202644d8784fd
qkrdudals <ympark@do-best.co.kr>
2025-02-18 16:25:22 +0900

    배치 시간 5분 으로 변경경

--- batch_script.py
+++ batch_script.py
@@ -146,7 +146,7 @@ def run_batch_job():
         print(f"배치 작업 실행 중 오류 발생: {e}")
 
 # 매일 1분마다 실행하도록 설정
-schedule.every(1).minutes.do(run_batch_job)
+schedule.every(5).minutes.do(run_batch_job)
 
 print("배치 작업이 스케줄링되었습니다. (매일 1분마다 실행)")
 
