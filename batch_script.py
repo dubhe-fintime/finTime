044243c7c2f721d5e7ee03949cd713e811505173
qkrdudals <ympark@do-best.co.kr>
2025-02-18 16:30:33 +0900

    TEST

--- batch_script.py
+++ batch_script.py
@@ -146,7 +146,7 @@ def run_batch_job():
 # 매일 1분마다 실행하도록 설정
 schedule.every(5).minutes.do(run_batch_job)
 
-print("배치 작업이 스케줄링되었습니다. (매일 1분마다 실행)")
+print("배치 작업이 스케줄링되었습니다. (매일 5분마다 실행)")
 
 # 무한 루프 실행 (배치 스케줄 유지)
 while True:
