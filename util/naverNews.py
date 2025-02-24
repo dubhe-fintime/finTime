689611dde199460a08e9dc289d9cb685c97c329b
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-24 16:39:05 +0900

    test

--- util/naverNews.py
+++ util/naverNews.py
@@ -19,36 +19,34 @@ def get_recent_news(targets: list):
     event_list = []
     url = "https://search.naver.com/search.naver"
     pattern = r"(?<=[\uac00-\ud7af])\."
-    try:
-        for target in targets:
-            print(target)
-            params = {
-                "where": "news",
-                "query": target
-            }
-            # 웹페이지 요청
-            response = requests.get(url, params=params)
-            soup = BeautifulSoup(response.text, "html.parser")
-            container = soup.find("ul", class_="list_news")
 
-            for element in container.find_all("li", class_="bx")[:5]:
-                result = re.split(pattern, element.find("div", class_="dsc_wrap").text.strip())
-                content_data = result[1] if len(result[0]) < 10 else result[0]
+    for target in targets:
+        print(target)
+        params = {
+            "where": "news",
+            "query": target
+        }
+        # 웹페이지 요청
+        response = requests.get(url, params=params)
+        soup = BeautifulSoup(response.text, "html.parser")
+        container = soup.find("ul", class_="list_news")
+        print(soup)
 
-                cor_no = next((item[0] for item in cor_gp if item[1] == target), None)
+        for element in container.find_all("li", class_="bx")[:5]:
+            result = re.split(pattern, element.find("div", class_="dsc_wrap").text.strip())
+            content_data = result[1] if len(result[0]) < 10 else result[0]
 
-                event_list.append({
-                    "press_name": element.find("a", class_=["info", "press"]).text,
-                    "press_img": element.find("img")["data-lazysrc"],
-                    "article_title": element.find("a", class_="news_tit").text,
-                    "article_content": content_data,
-                    "URL": element.find("a", class_="news_tit")["href"],
-                    "search_term": cor_no
-                })
-        return event_list
+            cor_no = next((item[0] for item in cor_gp if item[1] == target), None)
+
+            event_list.append({
+                "press_name": element.find("a", class_=["info", "press"]).text,
+                "press_img": element.find("img")["data-lazysrc"],
+                "article_title": element.find("a", class_="news_tit").text,
+                "article_content": content_data,
+                "URL": element.find("a", class_="news_tit")["href"],
+                "search_term": cor_no
+            })
+    return event_list
         
-    except Exception as e:
-        print(f"네이버 뉴스 크롤링 오류 발생: {e}")
-        return [{"ERROR": e}]
 
 get_recent_news(["신한은행", "국민은행"])
