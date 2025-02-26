65b932ff04f545754e271b0f1739e8f9c1189bd5
qkrdudals <ympark@do-best.co.kr>
2025-02-26 10:30:15 +0900

    test

--- main.py
+++ main.py
@@ -1636,7 +1636,8 @@ def updateSetting():
 @app.route('/getYouTube', methods=["POST"])
 async def getYouTube():
     youtube_key = config['SERVER']['youtube_key']
-    channels = ["신한은행", "우리은행", "국민은행", "하나은행", "NH농협은행"]
+    #channels = ["신한은행", "우리은행", "국민은행", "하나은행", "NH농협은행"]
+    channels = ["신한은행"]
 
     results = []
     for channel in channels:
