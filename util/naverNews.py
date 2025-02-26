e613d9032bf4a8c767317cf5d3949da872761ed1
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-26 09:08:03 +0900

    네이버수정중

--- util/naverNews.py
+++ util/naverNews.py
@@ -16,7 +16,6 @@ import dbconn
 
 async def get_recent_news(targets: list):
     session = requests.Session()
-    print(print(session.headers))
     cor_gp = dbconn.execute_mysql_query_select("Q22",(targets))
     event_list = []
     url = "https://search.naver.com/search.naver"
@@ -25,14 +24,18 @@ async def get_recent_news(targets: list):
     for target in targets:
         client_id = "BXpwyrG2dsvqVniOAIBI"
         client_secret = "dc9cDMiTL3"
-        requests.add_header("X-Naver-Client-Id",client_id)
-        requests.add_header("X-Naver-Client-Secret",client_secret)
+
+        header = {
+            "X-Naver-Client-Id":client_id,
+            "X-Naver-Client-Secret":client_secret
+        }
+
         params = {
             "where": "news",
             "query": target
         }
         # 웹페이지 요청
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params,headers=header)
         soup = BeautifulSoup(response.text, "html.parser")
         container = soup.find("ul", class_="list_news")
 
@@ -50,6 +53,7 @@ async def get_recent_news(targets: list):
                 "URL": element.find("a", class_="news_tit")["href"],
                 "search_term": cor_no
             })
+            
     return event_list
 
 
