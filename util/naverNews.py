133a4f3fd6f38b4f388d692b11b20b461b63da50
Choi-YoungHyun <cyounghyun07@gmail.com>
2025-02-26 10:21:24 +0900

    네이버 키 분리

--- util/naverNews.py
+++ util/naverNews.py
@@ -1,5 +1,5 @@
 import sys
-import time
+import configparser
 import os
 import re
 import requests
@@ -14,6 +14,16 @@ import dbconn
 # 언론사 이름 | 언론사 썸네일 | 기사 제목 | 기사 첫줄 | 기사 URL
 ##############################
 
+environment = os.getenv('ENVIRONMENT', 'development')
+if environment == 'production':
+    config_path = os.path.join(os.getcwd(), 'config.ini')
+    print(config_path)
+else:
+    config_path = os.path.join(os.getcwd(), 'config_development.ini')
+config = configparser.ConfigParser()
+config.read(config_path, encoding="utf-8")
+error = config['CODE']['error']
+
 
 async def get_recent_news(targets: list):
     session = requests.Session()
@@ -21,47 +31,49 @@ async def get_recent_news(targets: list):
     event_list = []
     url = "https://search.naver.com/search.naver"
     pattern = r"(?<=[\uac00-\ud7af])\."
-    client_id = "BXpwyrG2dsvqVniOAIBI"
-    client_secret = "dc9cDMiTL3"
-    
+
 
     header = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-            "X-Naver-Client-Id":"BXpwyrG2dsvqVniOAIBI",
-            "X-Naver-Client-Secret":"dc9cDMiTL3",
+            "X-Naver-Client-Id":config['NAVER']['ID'],
+            "X-Naver-Client-Secret":config['NAVER']['KEY'],
             "Referer": "https://admin.fin-time.com"
     }
-    # try:
-    for target in targets:
-        params = {
-            "where": "news",
-            "query": target
-        }
-        # 웹페이지 요청
-        response = requests.get(url, params=params,headers=header)
-        print(response.request.headers)
 
-        soup = BeautifulSoup(response.text, "html.parser")
-        print(soup)
-        container = soup.find("ul", class_="list_news")
-        print(container)
+    try:
+        for target in targets:
+            params = {
+                "where": "news",
+                "query": target
+            }
+            # 웹페이지 요청
+            response = requests.get(url, params=params,headers=header)
+            print(response.request.headers)
 
-        for element in container.find_all("li", class_="bx")[:5]:
-            result = re.split(pattern, element.find("div", class_="dsc_wrap").text.strip())
-            content_data = result[1] if len(result[0]) < 10 else result[0]
+            soup = BeautifulSoup(response.text, "html.parser")
+            print(soup)
+            container = soup.find("ul", class_="list_news")
+            print(container)
 
-            cor_no = next((item[0] for item in cor_gp if item[1] == target), None)
+            for element in container.find_all("li", class_="bx")[:5]:
+                result = re.split(pattern, element.find("div", class_="dsc_wrap").text.strip())
+                content_data = result[1] if len(result[0]) < 10 else result[0]
 
-            event_list.append({
-                "press_name": element.find("a", class_=["info", "press"]).text,
-                "press_img": element.find("img")["data-lazysrc"],
-                "article_title": element.find("a", class_="news_tit").text,
-                "article_content": content_data,
-                "URL": element.find("a", class_="news_tit")["href"],
-                "search_term": cor_no
-            })
-            
-    return event_list
+                cor_no = next((item[0] for item in cor_gp if item[1] == target), None)
+
+                event_list.append({
+                    "press_name": element.find("a", class_=["info", "press"]).text,
+                    "press_img": element.find("img")["data-lazysrc"],
+                    "article_title": element.find("a", class_="news_tit").text,
+                    "article_content": content_data,
+                    "URL": element.find("a", class_="news_tit")["href"],
+                    "search_term": cor_no
+                })
+                
+        return event_list
+    except Exception as e:
+        print(f"네이버뉴스 오류 발생: {e}")
+        return [{"ERROR": str(e)}]
 
 
         
