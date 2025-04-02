641393d50d042dbf36cb10099b5f15e62ff72380
qkrdudals <ympark@do-best.co.kr>
2025-04-02 09:18:35 +0900

    test

--- logMonitor.py
+++ logMonitor.py
@@ -1,26 +1,30 @@
 from flask import Flask
 from flask_sock import Sock
 import subprocess
+import ssl
 
 app = Flask(__name__)
 sock = Sock(app)
 
 LOG_FILE_PATH = "/home/finTime/logs/batch_log_20250402.log"
 
-# 특정 로그 파일을 실시간으로 읽어오는 함수
+# WebSocket에서 보낼 로그 파일
 def tail_log():
     process = subprocess.Popen(['tail', '-f', LOG_FILE_PATH], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     while True:
         line = process.stdout.readline()
         if not line:
             break
-        yield line.strip()  # 개행문자 제거 후 반환
+        yield line.strip()
 
-# 웹소켓을 통해 로그 스트리밍
 @sock.route('/logs')
 def logs(ws):
     for line in tail_log():
         ws.send(line)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8084, debug=True)
+    # SSL 설정 추가
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile='/etc/letsencrypt/live/fin-time.com/fullchain.pem', keyfile='/etc/letsencrypt/live/fin-time.com/privkey.pem')  # SSL 인증서 경로 지정
+
+    app.run(host='0.0.0.0', port=8084, debug=True, ssl_context=context)
