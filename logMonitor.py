54aea4c6e27fb850c338786f7dc42af4cb871387
qkrdudals <ympark@do-best.co.kr>
2025-04-02 10:23:28 +0900

    test

--- logMonitor.py
+++ logMonitor.py
@@ -1,32 +1,45 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_socketio import SocketIO
-import os
+from flask_cors import CORS
 import subprocess
+import os
+import configparser
+
+# 설정 파일 읽기
+script_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(script_dir, 'config.ini')
+config = configparser.ConfigParser()
+config.read(config_path)
+
+server_host = config['SERVER']['server_host']
+port2 = int(config['SERVER']['port_2'])
+ssl_cert = config['SECURE']['ssl_cert']
+ssl_key = config['SECURE']['ssl_key']
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # 모든 origin 허용
 
-LOG_DIR = "/home/finTime/logs"
+LOG_FILE_PATH = "/home/finTime/logs/batch_log_20250402.log"
 
+# WebSocket에서 보낼 로그 파일
+def tail_log():
+    process = subprocess.Popen(['tail', '-f', LOG_FILE_PATH], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        yield line.strip()
 
-# 특정 로그 파일을 실시간으로 스트리밍
+# WebSocket 이벤트 핸들러
+@socketio.on("connect")
+def handle_connect():
+    print("클라이언트 WebSocket 연결됨")
+
 @socketio.on("request_logs")
-def send_logs(data=None):
-    if not data or "filename" not in data:
-        socketio.emit("log_update", "⚠️ 로그 파일을 선택하세요.")
-        return
+def send_logs():
+    for line in tail_log():
+        socketio.emit("log_update", line)
 
-    filename = data["filename"]
-    log_path = os.path.join(LOG_DIR, filename)
-    print("1"*1000)
-    print(log_path)
-    if not os.path.exists(log_path):
-        socketio.emit("log_update", f"❌ 파일 '{filename}'이 존재하지 않습니다.")
-        return
-
-    process = subprocess.Popen(["tail", "-f", log_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    for line in process.stdout:
-        socketio.emit("log_update", line.strip())
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8084, allow_unsafe_werkzeug=True)
+if __name__ == '__main__':
+    socketio.run(app, host=server_host, port=port2, ssl_context=(ssl_cert, ssl_key), allow_unsafe_werkzeug=True)
